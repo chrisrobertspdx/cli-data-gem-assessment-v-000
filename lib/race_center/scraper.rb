8e69fc9718204735f202bd0d6b4063bb3044a369
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-08 21:59:50 +0000

    Done.

--- lib/race_center/scraper.rb
+++ lib/race_center/scraper.rb
@@ -4,10 +4,20 @@ require 'open-uri'
 
 class Scraper
   def get_page
-    Nokogiri::HTML(open("http://www.racecenter.com/race-calendar/"))
+    #Nokogiri::HTML(open("http://www.racecenter.com/race-calendar/"))
+    Nokogiri::HTML(open("http://www.gravelcyclist.com/calendar/"))
   end
   def scrape_races
-    puts self.get_page.css("table#calendarlist")
+    #self.get_page.css("table#calendarlist tr.r0,table#calendarlist tr.r1").each{|tr|
+      #tr.css("td").each_widthindex {|td,index|
+    events = self.get_page.css("div.ai1ec-event")
+    puts events.count
+    events.each {|e|
+      title = e.css("div.ai1ec-event-inner div.ai1ec-event-title a")
+      puts title.text.to_s.split.join(" ")
+      puts title.attribute("href").value
+      puts e.css("div.ai1ec-event-inner div.ai1ec-event-meta span.ai1ec-event-time").text.split.join(" ")
+    }
   end
 end
 
